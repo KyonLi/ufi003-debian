fabdea5c8fdcf230ab4ba077c17512cb6eb95e7d
Kyon Li <i@kyonli.com>
2025-10-02 14:22:25 +0800

    Debian 13

--- rootfs/build.sh
+++ rootfs/build.sh
@@ -1,11 +1,11 @@
 #/bin/bash
 
-DIST=bookworm
-BOOT_URL="https://github.com/KyonLi/ufi003-kernel/releases/download/6.6.60-1/boot.img"
-BOOT_NO_MODEM_URL="https://github.com/KyonLi/ufi003-kernel/releases/download/6.6.60-1/boot-no-modem.img"
-BOOT_NO_MODEM_OC_URL="https://github.com/KyonLi/ufi003-kernel/releases/download/6.6.60-1/boot-no-modem-oc.img"
-K_IMAGE_DEB_URL="https://github.com/KyonLi/ufi003-kernel/releases/download/6.6.60-1/linux-image-6.6.60-msm8916-g32b5a619bbf5_6.6.60-g32b5a619bbf5-1_arm64.deb"
-K_DEV_URL="https://github.com/KyonLi/ufi003-kernel/releases/tag/6.6.60-1"
+DIST=trixie
+BOOT_URL="https://github.com/KyonLi/ufi003-kernel/releases/download/6.12.49-1/boot.img"
+BOOT_NO_MODEM_URL="https://github.com/KyonLi/ufi003-kernel/releases/download/6.12.49-1/boot-no-modem.img"
+BOOT_NO_MODEM_OC_URL="https://github.com/KyonLi/ufi003-kernel/releases/download/6.12.49-1/boot-no-modem-oc.img"
+K_IMAGE_DEB_URL="https://github.com/KyonLi/ufi003-kernel/releases/download/6.12.49-1/linux-image-6.12.49-msm8916-g93a71ee9468d_6.12.49-g93a71ee9468d-1_arm64.deb"
+K_DEV_URL="https://github.com/KyonLi/ufi003-kernel/releases/tag/6.12.49-1"
 UUID=62ae670d-01b7-4c7d-8e72-60bcd00410b7
 
 if [ `id -u` -ne 0 ]
--- rootfs/chroot.sh
+++ rootfs/chroot.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
+DIST=trixie
 LANG_TARGET=en_US.UTF-8
 PASSWORD=1234
 NAME=ufi003
 PARTUUID=a7ab80e8-e9d1-e8cd-f157-93f69b1d141e
 
 cat <<EOF > /etc/apt/sources.list
-deb http://deb.debian.org/debian/ bookworm main contrib non-free non-free-firmware
-# deb-src http://deb.debian.org/debian/ bookworm main contrib non-free non-free-firmware
+deb http://deb.debian.org/debian/ $DIST main contrib non-free non-free-firmware
+# deb-src http://deb.debian.org/debian/ $DIST main contrib non-free non-free-firmware
 
-deb http://deb.debian.org/debian/ bookworm-updates main contrib non-free non-free-firmware
-# deb-src http://deb.debian.org/debian/ bookworm-updates main contrib non-free non-free-firmware
+deb http://deb.debian.org/debian/ $DIST-updates main contrib non-free non-free-firmware
+# deb-src http://deb.debian.org/debian/ $DIST-updates main contrib non-free non-free-firmware
 
-deb http://deb.debian.org/debian/ bookworm-backports main contrib non-free non-free-firmware
-# deb-src http://deb.debian.org/debian/ bookworm-backports main contrib non-free non-free-firmware
+deb http://deb.debian.org/debian/ $DIST-backports main contrib non-free non-free-firmware
+# deb-src http://deb.debian.org/debian/ $DIST-backports main contrib non-free non-free-firmware
 
-deb http://security.debian.org/debian-security bookworm-security main contrib non-free non-free-firmware
-# deb-src http://security.debian.org/debian-security bookworm-security main contrib non-free non-free-firmware
+deb http://security.debian.org/debian-security $DIST-security main contrib non-free non-free-firmware
+# deb-src http://security.debian.org/debian-security $DIST-security main contrib non-free non-free-firmware
 EOF
 
 cat <<EOF > /etc/fstab
